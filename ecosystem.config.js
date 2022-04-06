91ea4de933e5538e82f7f0d1bda50d6d177e7e81
AtlasRyan <ddtddt1998@bupt.cn>
2022-04-06 17:32:24 +0800

    修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
 		production: {
 			user: "root",
 			host: "47.94.199.134",
-			ref: "origin/master",
+			ref: "origin/main",
 			repo: "git@github.com:AtlasRyan/ReactAccount-FrontEnd.git",
 			path: "/workspace/ReactAccount-vite-h5",
 			"post-deploy":
-				"git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
+				"git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
 			env: {
 				NODE_ENV: "production",
 			},
